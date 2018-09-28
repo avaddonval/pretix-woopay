68178b55e767d689dd1f1180c6a7b3d30eeb25a3
Valeriy <avaddonval@gmail.com>
2018-09-28 15:15:54 +0600

    cascade one delete

--- woopay/models.py
+++ woopay/models.py
@@ -5,7 +5,7 @@ class ReferencedWoopayObject(models.Model):
     reference = models.CharField(max_length=190, db_index=True, unique=True)
     key = models.CharField(max_length=15, db_index=True, unique=True,null=True)
     url = models.TextField(null=True)
-    order = models.ForeignKey('pretixbase.Order',on_delete=models.DO_NOTHING)
+    order = models.ForeignKey('pretixbase.Order',on_delete=models.CASCADE)
 
 class SessionWoopayObject(models.Model):
     session = models.CharField(max_length=190, db_index=True, unique=True)
